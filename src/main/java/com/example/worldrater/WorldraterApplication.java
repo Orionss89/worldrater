92a4efd909afaf181e59febcdb9fd229268c2ca5
Orionss89 <bravura71@gmail.com>
2026-01-09 19:48:22 +0100

    Initial commit

---
+++ src/main/java/com/example/worldrater/WorldraterApplication.java
@@ -0,0 +1,13 @@
+package com.example.worldrater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldraterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldraterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/worldrater/WorldraterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worldrater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldraterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
