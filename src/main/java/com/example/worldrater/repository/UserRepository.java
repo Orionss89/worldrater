cedbe4196aaf93d146f1010dc7c4356d87e708ba
Orionss89 <bravura71@gmail.com>
2026-01-09 21:16:26 +0100

    feat: add repositories for User, Attraction and Review

---
+++ src/main/java/com/example/worldrater/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.worldrater.repository;
+
+import com.example.worldrater.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
